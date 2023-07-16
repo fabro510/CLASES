4c700aa998a3302e3ef63938d68040598b9671a7
Fabrizio Alejandro <caballerogame192837465@gmail.com>
2023-07-16 02:05:56 -0300

    trabajo 8 y 94

---
+++ intro_programacion/tema8/java/ejercicio8.java
@@ -0,0 +1,49 @@
+package intro_programacion.tema8.java;
+
+class ejercicio8{
+    public static void main(String[] args){
+        System.out.println("--------------------------------------------------");
+        System.out.println("ACTIVIDAD 1:");
+        personas persona = new personas();
+        persona.setNombre("Benjamin Olmos Cuello");
+        persona.setEdad(16);
+        persona.setTelefono(351268915);
+        System.out.println("                    ");
+        System.out.println(" Los datos de la persona se ingresaron correctamente");
+        System.out.println(" Se mostraran a continuacion:");
+        System.out.println(" -Nombre:" + persona.getNombre());
+        System.out.println(" -Edad:"+persona.getEdad());
+        System.out.println(" -Telefono:"+persona.getTelefono());
+        System.out.println("--------------------------------------------------");
+    }
+}
+
+class personas{
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public void setEdad(int edad){
+        this.edad=edad;
+        System.out.println(" /se ingreso edad");
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+
+    public void setTelefono(int telefono){
+        this.telefono=telefono;
+        System.out.println(" /se ingreso telefono");
+    }
+    public int getTelefono(){
+        return this.telefono;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+        System.out.println(" /se ingreso nombre");
+    }
+    public String getNombre(){
+            return this.nombre;
+        }
+}
---
+++ intro_programacion/tema9/java/ejercicio8.java
@@ -0,0 +1,58 @@
+package intro_programacion.tema9.java;
+
+public class ejercicio8 {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("ACTIVIDAD 1:");
+        cliente cliente1 = new cliente();
+        cliente1.nombre = "Olmos Benjamin Cuello";
+        System.out.println(" /se ingreso en nombre del cliente");
+        cliente1.edad = 16;
+        System.out.println(" /se ingreso la edad del cliente");
+        cliente1.telefono = 351268915;
+        System.out.println(" /se ingreso el telefono del cliente");
+        System.out.println(" ");
+        System.out.println(" Todos los datos se ingresaron correctamente");
+        cliente1.credito = 12000;
+        System.out.println(" /se cargo el credito del cliente");
+        System.out.println(" ");
+        System.out.println(" A continuacion se mostraran todos los datos finales del cliente:");
+        System.out.println(" -Nombre:"+cliente1.nombre);
+        System.out.println(" -Edad:"+cliente1.edad);
+        System.out.println(" -Telefono:"+cliente1.telefono);
+        System.out.println(" -Monto del credito:"+cliente1.credito);
+        System.out.println("--------------------------------------------------");
+        System.out.println("ACTIVIDAD 2:");
+        trabajador trabajador1 = new trabajador();
+        trabajador1.nombre = "Italo Montalvo Verdeneli";
+        System.out.println(" /se ingreso en nombre del empleado");
+        trabajador1.edad = 18;
+        System.out.println(" /se ingreso la edad del empleado");
+        trabajador1.telefono = 351593589;
+        System.out.println(" /se ingreso el telefono del empleado");
+        System.out.println(" ");
+        System.out.println(" Todos los datos se ingresaron correctamente");
+        trabajador1.salario = 40000;
+        System.out.println(" /se cargo el sueldo del cliente");
+        System.out.println(" ");
+        System.out.println(" A continuacion se mostraran todos los datos finales:");
+        System.out.println(" -Nombre:"+trabajador1.nombre);
+        System.out.println(" -Edad:"+trabajador1.edad);
+        System.out.println(" -Telefono:"+trabajador1.telefono);
+        System.out.println(" -Sueldo del trabajador"+trabajador1.salario);
+        System.out.println("--------------------------------------------------");
+    }
+}
+class persona{
+    String nombre;
+    int edad;
+    int telefono;
+}
+
+class cliente extends persona{
+    int credito;
+}
+
+class trabajador extends persona{
+    int salario;
+}
