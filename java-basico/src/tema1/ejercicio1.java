c9face93cfd9fc75efbd1abe700b89ba599e47a2
Fabrizio Alejandro <caballerogame192837465@gmail.com>
2023-07-20 20:39:42 -0300

    add : trabajo tema 1

---
+++ java-basico/src/tema1/ejercicio1.java
@@ -0,0 +1,32 @@
+package tema1;
+
+public class ejercicio1 {
+    
+
+    public static void main(String[] args) {
+        Byte a = 127;
+        short b = 32767;
+        int c =2147483647;
+        long d = 2147483647;
+
+        float e = 4.6f;
+        double f = 6.69d;
+
+        char g='a';
+        boolean h=false;
+        String i ="sexo";
+
+        Integer j =null;
+
+        System.out.println("variable byte: "+a);
+        System.out.println("variable short: "+b);
+        System.out.println("variable int: "+c);
+        System.out.println("variable long: "+d);
+        System.out.println("variable float: "+e);
+        System.out.println("variable double: "+f);
+        System.out.println("variable char: "+g);
+        System.out.println("variable booblean: "+h);
+        System.out.println("variable sring: "+i);
+        System.out.println("variable integer: "+j);
+    }
+}
