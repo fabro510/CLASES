fb79556bebb3b15b3abb18f1f54a60dba7468d16
Fabrizio Alejandro <caballerogame192837465@gmail.com>
2023-07-20 23:27:56 -0300

    add : trabajo 2 hecho

--- java-basico/src/tema2/ejercicio2.java
+++ java-basico/src/tema2/ejercicio2.java
@@ -1,7 +1,31 @@
 package tema2;
 
 public class ejercicio2 {
+    
     public static void main(String[] args) {
-        
+        barrera();
+        mostrar("ACTIVIDAD 1");
+        mostrar(" El porcentaje de iva es de: "+iva);
+        mostrar(" El monto a calcular es: "+precio);
+        int Total = calcularIva(precio);
+        mostrar(" El precio con iva incluido es: "+Total);
+        barrera();
+    }
+    static int precio=100;
+    static int iva=35;
+    static int calcularIva(int precio){
+        mostrar(" /calculando---");
+        precio+=precio*iva/100;
+        return precio;
+    }
+
+
+
+    
+    public static void mostrar(String texto){
+        System.out.println(texto);
+    }
+    static void barrera(){
+        System.out.println("--------------------------------");
     }
 }
