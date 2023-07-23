fd322f987086e462e6dcf6bd95fccab7c4d7abc9
Fabrizio Alejandro <caballerogame192837465@gmail.com>
2023-07-22 23:09:36 -0300

    add : ejercicio 3

---
+++ java-basico/src/tema3/ejercicio3.java
@@ -0,0 +1,16 @@
+package tema3;
+
+public class ejercicio3 {
+    public static void main(String[] args) {
+        String[] nombres={
+            "Lucía", "Mateo", "Sofía", "Gabriel", "Valentina", "Daniel", "Martina", "Alejandro", "Renata", "Emilio",
+        "Camila", "Isabella", "Joaquín", "Victoria", "Leonardo", "Mía", "David", "Olivia", "Juan", "Elena",
+        "Adrián", "Aitana", "Carlos", "Lola", "Manuel", "Ana", "Francisco", "Paula", "Mario", "Luciana",
+        "Fernando", "Valeria", "Raúl", "Florencia", "Andrés", "Clara", "Gustavo", "Mara", "Pablo", "Emma",
+        "Diego", "Antonella", "Sebastián", "Catalina", "Ricardo", "Julia", "Javier", "Eva", "José", "Ángela"
+        };
+        for(int i=0;i<nombres.length;i++){
+            System.out.println(nombres[i]);
+        }
+    }
+}
